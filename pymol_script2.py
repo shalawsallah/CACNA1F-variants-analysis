cca885c9e9805a2c0b2744cad2810d09bca08410
shalawsallah <41365954+shalawsallah@users.noreply.github.com>
2019-10-14 15:37:02 +0100

    Update and rename variant_list.py to pymol_script2.py

--- pymol_script2.py
+++ pymol_script2.py
@@ -5,7 +5,7 @@
 PYMOL SCRIPT 2.
 
 inputing the variant numbers to output in the format required in PyMol command line.
-beforehand, residue numbers are extracted using 'extract_resi_numbers.py' & copied into 'pymol_test_residues.txt' for each gene separately
+beforehand, residue numbers are extracted using 'extract_resi_numbers.py' & copied into 'pymol_test_residues.txt'
 '''
 
 f1=open('/Users/mdefsss2/PycharmProjects/spatial_distribution/pymol_visualisation/pathogenic_CACNA1F_resNumbers.txt')
